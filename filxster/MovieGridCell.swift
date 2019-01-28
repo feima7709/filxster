52b99594bcbb758816e0b62274110c51af5bd842
fma1@uci.edu <mafei@feis-macbook-pro.local>
2019-01-27 23:23:41 -0800

    superhero page

---
+++ filxster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  filxster
+//
+//  Created by Fei Ma on 1/27/19.
+//  Copyright © 2019 Fei Ma. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- filxster/MoviesViewController.swift
+++ filxster/MoviesViewController.swift
@@ -26,8 +26,12 @@ UITableViewDelegate{
         // Do any additional setup after loading the view, typically from a nib.
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
+        let session =
+            URLSession(configuration: .default,
+                       delegate: nil, delegateQueue:
+                OperationQueue.main)
+        let task = session.dataTask(with: request)
+        { (data, response, error) in
             // This will run when the network request returns
             if let error = error {
                 print(error.localizedDescription)
@@ -46,7 +50,8 @@ UITableViewDelegate{
         }
         task.resume()
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
     
@@ -60,14 +65,32 @@ UITableViewDelegate{
         cell.titleLable.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "http://image.tmdb.org/t/p/w185"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
+        
         cell.posterView.af_setImage(withURL:posterUrl!)
         
         return cell
     }
-
+    override func prepare(for segue:UIStoryboardSegue,sender:Any?)
+    {
+        print("loading up")
+        
+        //find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        
+        //pass the slected movie to the details view cintroller
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated:true)
+    }
 
 }
 
